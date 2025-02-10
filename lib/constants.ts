46634c0bdaa9e8db7a2ab9f5f9c4d27182ed418c
smaranshaha <smaranshaha@gmail.com>
2025-02-10 21:33:13 +0600

    added product detail page and here you see multiple images and also see the related product of the main product

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,4 @@ export const APP_SLOGAN =
   process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, enjoy more.";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Built with Next.js and MongoDB";
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
