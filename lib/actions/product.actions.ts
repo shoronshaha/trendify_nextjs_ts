47fc8954580305f76044ba4de86b8af4f0ba0c4f
smaranshaha <smaranshaha@gmail.com>
2025-02-10 19:12:48 +0600

    added products card and carousel and here customer see the discunt percenteges and price also, all the the data are fecth from mongodb

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -34,3 +34,21 @@ export async function getProductsForCard({
     image: string;
   }[];
 }
+
+// GET PRODUCTS BY TAG
+export async function getProductsByTag({
+  tag,
+  limit = 10,
+}: {
+  tag: string;
+  limit?: number;
+}) {
+  await connectToDatabase();
+  const products = await Product.find({
+    tags: { $in: [tag] },
+    isPublished: true,
+  })
+    .sort({ createdAt: "desc" })
+    .limit(limit);
+  return JSON.parse(JSON.stringify(products)) as IProduct[];
+}
