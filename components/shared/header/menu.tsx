529a8fc3b96a5bc17f31dffd5698ce85e61f7406
smaranshaha <smaranshaha@gmail.com>
2025-02-11 12:47:13 +0600

    here added use cart there are now user can see his added orderd history what are they added in the press of cart icoon

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -6,10 +6,12 @@ export default function Menu() {
   return (
     <div className="flex justify-end">
       <nav className="flex gap-3 w-full">
-        <Link href="/signin" className="header-button">
-          <UserIcon className="h-8 w-8" />
-          <span className="font-bold">Sign in</span>
-        </Link>
+        <div className="flex items-end text-xs relative">
+          <Link href="/signin" className="px-1 flex header-button">
+            <UserIcon className="h-8 w-8" />
+            <span className="font-bold">Sign in</span>
+          </Link>
+        </div>
 
         <CartButton />
       </nav>
