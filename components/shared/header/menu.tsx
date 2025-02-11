5fe53077a768e2cdc36d7426325bc39f27219884
smaranshaha <smaranshaha@gmail.com>
2025-02-11 13:03:09 +0600

    added view cart

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -6,12 +6,10 @@ export default function Menu() {
   return (
     <div className="flex justify-end">
       <nav className="flex gap-3 w-full">
-        <div className="flex items-end text-xs relative">
-          <Link href="/signin" className="px-1 flex header-button">
-            <UserIcon className="h-8 w-8" />
-            <span className="font-bold">Sign in</span>
-          </Link>
-        </div>
+        <Link href="/signin" className="header-button flex text-sm">
+          <UserIcon className="h-8 w-8" />
+          <span className="font-bold">Sign in</span>
+        </Link>
 
         <CartButton />
       </nav>
